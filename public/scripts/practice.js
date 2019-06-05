33ac228ad716a73cf2c13fddceba2e173fc57836
Bhanu-12 <Shrivastava1219@gmail.com>
2019-06-05 14:48:57 +0530

    Conditional rendering

--- public/scripts/practice.js
+++ public/scripts/practice.js
@@ -3,7 +3,33 @@
 console.log("App.js is running");
 var book = {
   title: "The Song Of Ice ANd Fire",
-  Subtitle: "Robert's Rebellion"
+  Subtitle: "Robert's Rebellion",
+  options: ['One', 'Two']
+};
+var getSubtitle = function getSubtitle(Subtitle) {
+  if (Subtitle) {
+    return React.createElement(
+      "p",
+      null,
+      "Subtitle: ",
+      Subtitle
+    );
+  }
+};
+var getOptions = function getOptions(options) {
+  if (options.length > 0) {
+    return React.createElement(
+      "p",
+      null,
+      "Here are your options"
+    );
+  } else {
+    return React.createElement(
+      "p",
+      null,
+      "There are no options"
+    );
+  }
 };
 var template = React.createElement(
   "div",
@@ -13,11 +39,8 @@ var template = React.createElement(
     null,
     book.title
   ),
-  React.createElement(
-    "p",
-    null,
-    book.Subtitle
-  )
+  getSubtitle(book.Subtitle),
+  getOptions(book.options)
 );
 
 var practiceRoot = document.getElementById("app");
--- src/practice.js
+++ src/practice.js
@@ -1,12 +1,26 @@
 console.log("App.js is running");
 var book = {
   title: "The Song Of Ice ANd Fire",
-  Subtitle: "Robert's Rebellion"
+  Subtitle: "Robert's Rebellion",
+  options: ['One','Two']
+};
+let getSubtitle = (Subtitle)=>{
+  if(Subtitle){
+    return <p>Subtitle: {Subtitle}</p>
+  }
+};
+let getOptions = (options)=>{
+  if(options.length>0){
+    return <p>Here are your options</p>
+  }else{
+    return <p>There are no options</p>
+  }
 };
 var template = (
   <div>
     <h1>{book.title}</h1>
-    <p>{book.Subtitle}</p>
+    {getSubtitle(book.Subtitle)}
+    {getOptions(book.options)}
   </div>
 );
 
