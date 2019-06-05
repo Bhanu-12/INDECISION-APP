fa10d887241284fddf325c7d517fe2e50f2ab1db
Bhanu-12 <Shrivastava1219@gmail.com>
2019-06-06 03:03:20 +0530

    adding forms and inputs

---
+++ public/scripts/app.js
@@ -0,0 +1,65 @@
+"use strict";
+
+var appRoot = document.getElementById("app");
+var app = {
+  title: "INDECISION APP",
+  subtitle: "Put your life in hands of a computer",
+  options: []
+};
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  var option = e.target.elements.option.value;
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = "";
+    renderFunction();
+  }
+};
+var renderFunction = function renderFunction() {
+  var template = React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h1",
+      null,
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      "p",
+      null,
+      "app.subtitle"
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.options.length > 0 ? "Here are your OPtions" : "No options"
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.options.length
+    ),
+    React.createElement(
+      "button",
+      {
+        onClick: function onClick() {
+          app.options = [];
+          renderFunction();
+        }
+      },
+      "Remove All"
+    ),
+    React.createElement(
+      "form",
+      { onSubmit: onFormSubmit },
+      React.createElement("input", { type: "text", name: "option" }),
+      React.createElement(
+        "button",
+        null,
+        "Add option"
+      )
+    )
+  );
+  ReactDOM.render(template, appRoot);
+};
+renderFunction();
---
+++ src/app.js
@@ -0,0 +1,39 @@
+const appRoot = document.getElementById("app");
+const app = {
+  title: "INDECISION APP",
+  subtitle: "Put your life in hands of a computer",
+  options: []
+};
+const onFormSubmit = e => {
+  e.preventDefault();
+  const option = e.target.elements.option.value;
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = "";
+    renderFunction();
+  }
+};
+const renderFunction = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>app.subtitle</p>}
+      <p>{app.options.length > 0 ? "Here are your OPtions" : "No options"}</p>
+      <p>{app.options.length}</p>  
+      <button
+        onClick={() => {
+          app.options = [];
+          renderFunction();
+        }}
+      >
+        Remove All
+      </button>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add option</button>
+      </form>
+    </div>
+  );
+  ReactDOM.render(template, appRoot);
+};
+renderFunction();
