8df8ac34428876b6f66d79a4815621a3ad86ee5d
Bhanu-12 <Shrivastava1219@gmail.com>
2019-06-10 15:33:43 +0530

    webpack change

--- src/import/app.js
+++ src/import/app.js
@@ -1,5 +1,9 @@
-import isSenior, { isAdult, canDrink }  from "./utils";
-console.log("App is running!!");
-console.log(isAdult(20));
-console.log(canDrink(20));
-console.log(isSenior(65));
+// import isSenior, { isAdult, canDrink }  from "./utils";
+// console.log("App is running!!");
+// console.log(isAdult(20));
+// console.log(canDrink(20));
+// console.log(isSenior(65));
+import React from "react";
+import ReactDOM from "react-dom";
+const template = <p>This is JSX</p>;
+ReactDOM.render(template, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require("path");
-console.log(path.join(__dirname, "public"));
+// console.log(path.join(__dirname, "public"));
 module.exports = {
   entry: "./src/import/app.js",
   mode: "none",
   output: {
     path: path.join(__dirname, "public"),
     filename: "bundle.js"
+  },
+  module: {
+    rules:[{
+      loader:'babel-loader',
+      test:/\.js$/,
+      exclude:/node_modules/,
+    }]
   }
 };
