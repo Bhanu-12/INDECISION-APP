2a0295374ed2515407986cddc562ef36c1a6c0a2
Bhanu-12 <Shrivastava1219@gmail.com>
2019-06-18 18:42:34 +0530

    changes

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -25,8 +25,17 @@ module.exports = {
   optimization: {
     minimizer: [
       new UglifyJsPlugin({
-        sourceMap: false,
-        mangle: false
+        uglifyOptions: {
+          warnings: false,
+          parse: {},
+          compress: {},
+          mangle: true, // Note `mangle.properties` is `false` by default.
+          output: null,
+          toplevel: false,
+          nameCache: null,
+          ie8: false,
+          keep_fnames: false
+        }
       })
     ]
   }
