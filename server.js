26466a0305e5b9988ad005d05ea01673c9f3acfb
Bhanu-12 <Shrivastava1219@gmail.com>
2019-06-18 19:12:38 +0530

    removing fault in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express =require("express");
 const app = express();
 const publicPath = path.join(__dirname, "..", "public");
 const port = process.env.PORT || 3000;
-const numeral = require("num")
+
 app.use(express.static(publicPath));
 
 app.get("*", (req, res) => {
